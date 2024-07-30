a5ba8728efa1ea3e40c5303890cf19c6ec4e5665
yhanz15 <145173842+yhanz15@users.noreply.github.com>
2024-07-30 14:09:10 +0000

    Deploying to gh-pages from @ yhanz15/goit-react-hw-08-phonebook@104c9856506cfe403c127edcc9c2659d9bff54e7 🚀

---
+++ static/js/856.fe22ed22.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[856],{6856:function(t,e,a){a.d(e,{ZkW:function(){return r}});var h=a(9983);function r(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}}]})(t)}}}]);
+//# sourceMappingURL=856.fe22ed22.chunk.js.map
